38a21fdcbc49b7cb47ed7c67f67633a8b32bdd29
Chocobo <skylinkbox@gmail.com>
2018-06-05 00:08:53 +0200

    tactileOK

--- core/src/com/pacman/game/controller/DirectionListener.java
+++ core/src/com/pacman/game/controller/DirectionListener.java
@@ -17,6 +17,7 @@ public class DirectionListener implements  GestureListener {
     private void update(Vector2 vel){
         _world.getPacman().setNextVelocity(vel);
     }
+    //dd
 
     @Override
     public boolean touchDown(float x, float y, int pointer, int button) {
